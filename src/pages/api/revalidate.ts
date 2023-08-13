3da5fd1062a3bc81e37daf4817c0fad07e28469d
mishio-n <doncool340@gmail.com>
2023-08-13 19:27:34 +0900

    revalidateAPIをpages routerに戻す

---
+++ src/pages/api/revalidate.ts
@@ -0,0 +1,17 @@
+import { NextApiHandler } from "next";
+
+const revalidate: NextApiHandler = async (req, res) => {
+  if (req.method !== "POST") {
+    return res.status(400).end();
+  }
+
+  const paths = req.body.paths as string[];
+
+  for (const path of paths) {
+    await res.revalidate(path);
+  }
+
+  return res.json({ revalidated: true });
+};
+
+export default revalidate;
